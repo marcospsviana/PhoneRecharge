dcda9e4db4cf9cbc263e598c011bca80d32b133b
marcos p s viana <marcospaulo.silvaviana@gmail.com>
2021-10-08 18:27:48 -0300

    Developer (#13)
    * add workflow phonecharge-app.yml
    
    * alterado models para obter id's pelo public_id
    
    * add environments phonecharge-app.yml
    
    * add create db  phonecharge-app.yml
    
    * add environment SQLALCHEMY_DATABASE_URI
    
    * add coveragerc
    
    * alterado ci para apenas PR
    
    * alterado README, add coverage badge
    
    * alterado ci pytest cov
    
    * alterado models para orm
    
    * query de select products

--- phonecharge/tests/test_app.py
+++ phonecharge/tests/test_app.py
@@ -1,2 +1,16 @@
+import json
+
+
 def test_get_root_endpoint(client, db):
     assert client.get("/").status_code == 404
+
+
+def test_get_endpoint_products(client, db):
+    assert client.get("/CompanyProducts")
+    
+       
+
+def test_get_endpoint_product_id(client, db):
+    print(f"products {db}")
+    assert client.get("/CompanyProducts")
+   
