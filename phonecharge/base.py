3fb4d5119ed4350b28cfbfb93ceb5e5dd6ffaf25
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-10-11 10:08:58 -0300

    created charge create, select charge, select product

--- phonecharge/base.py
+++ phonecharge/base.py
@@ -4,7 +4,7 @@ from decouple import config  # config environment
 from flask_migrate import Migrate
 from flask_restful import Api
 
-from phonecharge.api import CompanyProducts
+from phonecharge.api import CompanyProducts, Recharge
 
 db = SQLAlchemy()
 
@@ -15,9 +15,12 @@ def create_app():
         config("SECRET_KEY"),
         config("SQLALCHEMY_DATABASE_URI"),
     )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     api = Api(app=app)
-    api.add_resource(CompanyProducts, "/CompanyProducts", provide_automatic_options=True)
+    api.add_resource(
+        CompanyProducts, "/CompanyProducts", provide_automatic_options=True
+    )
+    api.add_resource(Recharge, "/PhoneRecharges")
 
     migrate = Migrate(app, db)
     db.init_app(app)
--- phonecharge/operations/create.py
+++ phonecharge/operations/create.py
@@ -1,4 +1,6 @@
+import json
 from phonecharge.models import Product, Recharge
+import uuid
 
 
 def save_company(company):
@@ -12,19 +14,22 @@ def save_company(company):
 def save_product(product):
     product_data = Product(product["company_id"], product["id"], product["value"])
     try:
-        session.save(product_data)
+        product_data.save()
     except Exception as err:
         return err
 
 
 def save_recharge(recharge):
+    uid_recharge = str(uuid.uuid4().int)
+    print(f'recharge {json.loads(recharge)}')
+    recharge = json.loads(recharge)
     recharge_data = Recharge(
-        recharge["company_id"],
-        recharge["product_id"],
-        recharge["phone_number"],
-        recharge["value"],
+            public_id=uid_recharge,
+            product_id=recharge["product_id"],
+            phone_number=recharge["phone_number"],
+            value=recharge["value"]
     )
     try:
-        session.save(recharge_data)
+        recharge_data.save()
     except Exception as err:
         return err
