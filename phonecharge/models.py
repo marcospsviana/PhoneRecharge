9a8bcf776ad46cb8f872f01a5b23b65a0da39533
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-10-13 13:52:45 -0300

    retornado SQLALCHEMY_DATABASE_URI env

--- phonecharge/models.py
+++ phonecharge/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP, DECIMAL
 from sqlalchemy.orm import sessionmaker, relationship
 from passlib.context import CryptContext
 
-engine = create_engine(config("DATABASE_URL"))
+engine = create_engine(config("SQLALCHEMY_DATABASE_URI"))
 
 Session = sessionmaker(bind=engine)
 session = Session()
