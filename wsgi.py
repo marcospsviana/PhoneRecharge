4c89f2c0af1fa2239086e0ee50e7f30392588dc6
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-10-14 13:27:44 -0300

    procfile add quote command create_app

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from phonecharge.base import create_app
-from decouple import config
-
-
-if __name__ == "__main__":
-    DEBUG = config("DEBUG", default=False, cast=bool)
-    create_app().run(debug=False)
